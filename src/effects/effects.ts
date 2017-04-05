bd501b7754f3f7d29f8fe2ebf9a79888150f0127
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-04-05 20:38:23 +0800

    fix effect types

--- src/effects/effects.ts
+++ src/effects/effects.ts
@@ -2,18 +2,18 @@ import * as R from 'ramda';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 // import { of } from 'rxjs/observable/of';
-import { MyAction } from '../actions/actions';
+import { MyAction, ActionCtor, ActionPair } from '../actions/actions';
 
 export let unJson = <T>(resp: Response): T => resp.json();
 // export let always = R.pipe(R.always, Observable.of);
 
 // context dependencies: this.actions$: Actions;
-export let makeEffect = <T,U,V,W>(pair: ActionPair<T>, completeAction: ActionCtor<U>, mapper: (v: T) => { obs: Observable<U>, complete?: V, fail?: W }, opts: {
+export let makeEffect = <T,U,V,W>(pair: ActionPair<T>, okAction: ActionCtor<U|V>, mapper: (v: T) => { obs: Observable<U>, ok?: (v: U) => V, fail?: (e: Error) => W }, opts: {
   init?: T,  // trigger this effect immediately on startup
   debounce?: number, // 0
   read?: boolean,  // cancel previous results (good for read: GET, not for write: POST/PUT/PATCH/DELETE)
   fallback?: U,  // default value rather than failure
-  failAction?: ActionCtor<Error>,
+  failAction?: ActionCtor<Error|W>,
 } = {}) => {
   let { init, debounce, read, fallback, failAction } = opts;
   let actions$: Actions = this.actions$;
@@ -23,10 +23,10 @@ export let makeEffect = <T,U,V,W>(pair: ActionPair<T>, completeAction: ActionCto
       .debounceTime(debounce || 0)
       .map(R.prop('payload'));
   let lambda = (v: T) => {
-    let { obs: Observable<U>, complete?: V, fail?: W } = mapper(v);
+    let { obs, ok, fail } = mapper(v);
     let defaulted$ = !R.isNil(fallback) ? obs.catch(() => Observable.of(<U> fallback)) : obs;
-    let created$ = defaulted$.map((d: U) => new completeAction(complete || d));
-    let caught$ = failAction /*instanceof Object*/ ? created$.catch((error: Error) => Observable.of(new (<ActionCtor<Error>>failAction)(fail || error))) : created$;
+    let created$ = defaulted$.map((v: U) => new okAction(ok ? ok(v) : v));
+    let caught$ = failAction /*instanceof Object*/ ? created$.catch((error: Error) => Observable.of(new (<ActionCtor<Error|W>>failAction)(fail ? fail(error) : error))) : created$;
     return caught$;
   };
   return read ? payload$.switchMap(lambda) : payload$.mergeMap(lambda);

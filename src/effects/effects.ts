37e6cab1ceb6bff54882f695d97e9d4ac665d33e
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-04-07 18:35:36 +0800

    allow effects to drop obs wrapper

--- src/effects/effects.ts
+++ src/effects/effects.ts
@@ -8,26 +8,44 @@ export let unJson = <T>(resp: Response): T => resp.json();
 // export let always = R.pipe(R.always, Observable.of);
 
 // context dependencies: this.actions$: Actions;
-export let makeEffect = <T,U,V,W>(pair: ActionPair<T>, okAction: ActionCtor<U|V>, mapper: (v: T) => { obs: Observable<U>, ok?: (v: U) => V, fail?: (e: Error) => W }, opts: {
-  init?: T,  // trigger this effect immediately on startup
-  debounce?: number, // 0
-  read?: boolean,  // cancel previous results (good for read: GET, not for write: POST/PUT/PATCH/DELETE)
-  fallback?: U,  // default value rather than failure
-  failAction?: ActionCtor<Error|W>,
+export let makeEffect = <T,U,V,W>(
+  pair: ActionPair<T>,
+  okAction: ActionCtor<U|V>,
+  mapper: (v: T) => Observable<U> | { obs: Observable<U>, ok?: (v: U) => V, fail?: (e: Error) => W },
+  opts: {
+    init?: T,  // trigger this effect immediately on startup
+    debounce?: number, // 0
+    read?: boolean,  // cancel previous results (good for read: GET, not for write: POST/PUT/PATCH/DELETE)
+    fallback?: U,  // default value rather than failure
+    failAction?: ActionCtor<Error|W>,
 } = {}) => {
   let { init, debounce, read, fallback, failAction } = opts;
   // let actions$: Actions = this.actions$;
-  let actions$: Actions = this.actions$;
-  let filtered$: Observable<MyAction<T>> = actions$.ofType(pair.type); // could the exact action be inferred using a string-based lookup?
+  let actions$: /*Actions*/Observable<any> = this.actions$;
+  let filtered$: Observable<MyAction<T>> = actions$.ofType(pair.type);
+  // ^ could the exact action be inferred using a string-based lookup?
   let initialized$ = init ? filtered$.startWith(new (pair.action)(init)) : filtered$;
   let payload$ = initialized$
       .debounceTime(debounce || 0)
       .map(R.prop('payload'));
   let lambda = (v: T) => {
-    let { obs, ok, fail } = mapper(v);
+    let res = mapper(v);
+    let obs: Observable<U>;
+    let ok: (v: U) => V;
+    let fail: (e: Error) => W;
+    if (res instanceof Observable) {
+      obs = res;
+    } else {
+      // var { obs, ok, fail } = res;
+      obs = res.obs;
+      ok = res.ok;
+      fail = res.fail;
+    }
     let defaulted$ = !R.isNil(fallback) ? obs.catch(() => Observable.of(<U> fallback)) : obs;
     let created$ = defaulted$.map((v: U) => new okAction(ok ? ok(v) : v));
-    let caught$ = failAction /*instanceof Object*/ ? created$.catch((error: Error) => Observable.of(new (<ActionCtor<Error|W>>failAction)(fail ? fail(error) : error))) : created$;
+    let caught$ = failAction /*instanceof Object*/ ? created$.catch((error: Error) =>
+      Observable.of(new (<ActionCtor<Error|W>>failAction)(fail ? fail(error) : error))
+    ) : created$;
     return caught$;
   };
   return read ? payload$.switchMap(<any>lambda) : payload$.mergeMap(<any>lambda);
